cf9a46f6cdf5cfda91a07201f0d96e0e547143cb
Mona233 <Mona233@users.noreply.github.com>
2015-09-07 15:05:55 +0200

    Delete logout_view.php

--- application/views/auth/logout_view.php
+++
@@ -1,8 +0,0 @@
-
-    <?php if($logged_in): ?>
-      <p><?= $username ?> has been logged out.</p>
-      <p>Thanks for visiting <?= $name ?></p>
-    <?php else: ?>
-      <p>You need to <?php echo anchor('/auth/login', 'login', ''); ?> before you log out...</p>
-    <?php endif;?>
-
